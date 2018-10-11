a12070fd1c9f28873ac176899b11be3666d66752
ernestst <44066820+ernestst@users.noreply.github.com>
2018-10-11 20:24:19 +0200

    My version of stack in java
    Base Stack in java with base function.

--- stack/java/stack.java
+++ stack/java/stack.java
@@ -1,1 +1,54 @@
-// add your code here :
+#include<iostream>
+using namespace std;
+
+class Stosik{
+private:
+int *Stos;
+int which=0;
+public:
+    int how_much;
+void init(){
+    Stos=new int[how_much];
+}
+
+void push(int number){
+    if(which==how_much) cout<<"cant add, stack is full";
+    else{
+        Stos[which]=number;
+        which++;
+    }
+}
+
+int pop(){
+    int number;
+    if(which==0) cout<<"it is no data in stack";
+    else{
+        number=Stos[which-1];
+        Stos[which-1]=NULL;
+        which--;
+    }
+    return number;
+}
+
+void destroy(){
+    delete[] Stos;
+}
+
+int top(){
+if(which==0) cout<<"it is no data in this stack";
+    else{
+        return Stos[which-1];
+    }
+}
+
+bool empty(){
+    if(which==0) return true;
+    else return false;
+}
+
+bool full(){
+    if(which==how_much) return true;
+    else return false;
+}
+
+};
